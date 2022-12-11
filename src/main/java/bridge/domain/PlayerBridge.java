41657b9c8d2b81064783e411e9dc52845c08edd6
ksh-beginner <ohk9134@naver.com>
2022-12-11 17:25:09 +0900

    feat(PlayerBridge) : 플레이어 이동 기능 구현

---
+++ src/main/java/bridge/domain/PlayerBridge.java
@@ -0,0 +1,69 @@
+package bridge.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+public class PlayerBridge {
+
+    private static final String UP_SHAPE = "U";
+    private static final String DOWN_SHAPE = "D";
+    private static final String SUCCESS_SHAPE = "O";
+    private static final String FAILURE_SHAPE = "X";
+    private static final String BLANK_SHAPE = " ";
+
+    private final List<String> upShape = new ArrayList<>();
+    private final List<String> downShape = new ArrayList<>();
+
+    public void move(String moveShape, boolean canMove) {
+        if (canMove) {
+            moveSuccess(moveShape);
+        }
+        if (!canMove) {
+            moveFailure(moveShape);
+        }
+    }
+
+    private void moveSuccess(String moveShape) {
+        if (moveShape.equals(UP_SHAPE)) {
+            addUpShape(SUCCESS_SHAPE);
+            addDownShape(BLANK_SHAPE);
+        }
+        if (moveShape.equals(DOWN_SHAPE)) {
+            addDownShape(SUCCESS_SHAPE);
+            addUpShape(BLANK_SHAPE);
+        }
+    }
+
+    private void moveFailure(String moveShape) {
+        if (moveShape.equals(UP_SHAPE)) {
+            addUpShape(FAILURE_SHAPE);
+            addDownShape(BLANK_SHAPE);
+        }
+        if (moveShape.equals(DOWN_SHAPE)) {
+            addDownShape(FAILURE_SHAPE);
+            addUpShape(BLANK_SHAPE);
+        }
+    }
+
+    private void addUpShape(String shapeToAdd) {
+        upShape.add(shapeToAdd);
+    }
+
+    private void addDownShape(String shapeToAdd) {
+        downShape.add(shapeToAdd);
+    }
+
+    public int getCurrentBridgeSize() {
+        return upShape.size();
+    }
+
+    public List<String> getCurrentUpShape() {
+        return Collections.unmodifiableList(this.upShape);
+    }
+
+    public List<String> getCurrentDownShape() {
+        return Collections.unmodifiableList(this.downShape);
+    }
+}
