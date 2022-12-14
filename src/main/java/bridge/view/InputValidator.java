016d225b4bb3779811dc535e5a8000af3c3ea5b4
ksh-beginner <ohk9134@naver.com>
2022-12-14 21:46:12 +0900

    feat(InputValidator) : 다리 길이가 3이상 20이하가 아니면 예외 처리 구현

--- src/main/java/bridge/view/InputValidator.java
+++ src/main/java/bridge/view/InputValidator.java
@@ -6,15 +6,25 @@ import java.util.regex.Pattern;
 
 public class InputValidator {
 
-    private static final String DIGIT_REGEX = "[0-9]+";
+    private static final String DIGIT_REGEX = "[-+]*[0-9]+";
+    private static final int MIN_BRIDGE_SIZE = 3;
+    private static final int MAX_BRIDGE_SIZE = 20;
 
     public void validateBridgeSize(String bridgeSize) {
         if (isBridgeSizeNotDigit(bridgeSize)) {
             throw new IllegalArgumentException(ErrorConstants.ERROR_PREFIX + "다리 길이는 숫자여야합니다.");
         }
+        if (isWrongBridgeSize(bridgeSize)) {
+            throw new IllegalArgumentException(ErrorConstants.ERROR_PREFIX + "다리 길이는 3이상 20이하여야합니다.");
+        }
     }
 
     private boolean isBridgeSizeNotDigit(String bridgeSize) {
         return !Pattern.matches(DIGIT_REGEX, bridgeSize);
+    }
+
+    private boolean isWrongBridgeSize(String bridgeSize) {
+        int convertedBridgeSize = Integer.parseInt(bridgeSize);
+        return convertedBridgeSize < MIN_BRIDGE_SIZE || convertedBridgeSize > MAX_BRIDGE_SIZE;
     }
 }
--- src/test/java/bridge/view/InputValidatorTest.java
+++ src/test/java/bridge/view/InputValidatorTest.java
@@ -20,4 +20,14 @@ class InputValidatorTest {
                 .hasMessage(ErrorConstants.ERROR_PREFIX + "다리 길이는 숫자여야합니다.");
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "-1", "21", "100"})
+    void 다리_길이가_3이상_20이하가_아니면_예외_처리(String bridgeSize) {
+        InputValidator inputValidator = new InputValidator();
+
+        assertThatThrownBy(() -> inputValidator.validateBridgeSize(bridgeSize))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorConstants.ERROR_PREFIX + "다리 길이는 3이상 20이하여야합니다.");
+    }
+
 }
