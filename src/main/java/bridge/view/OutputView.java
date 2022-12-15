1f8aef7c36713a6fc71a6d8a4487258c2104dcf3
ksh-beginner <ohk9134@naver.com>
2022-12-16 00:13:56 +0900

    feat(OutputView) : 게임 재시도/종료 입력 문구 출력 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -8,6 +8,7 @@ public class OutputView {
     private static final String BRIDGE_GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
     private static final String READ_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
     private static final String READ_MOVING_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String READ_RETRY_OR_END_COMMAND_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     public void printBridgeGameStartMessage() {
         printMessage(BRIDGE_GAME_START_MESSAGE);
@@ -20,7 +21,12 @@ public class OutputView {
 
     public void printReadMovingMessage() {
         printBlankLine();
-        printReadMovingMessage();
+        printMessage(READ_MOVING_MESSAGE);
+    }
+
+    public void printReadRetryOrEndCommandMessage() {
+        printBlankLine();
+        printMessage(READ_RETRY_OR_END_COMMAND_MESSAGE);
     }
 
     /**
