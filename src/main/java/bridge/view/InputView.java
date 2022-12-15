1fcdcb2b62e23ac0ae77aba26e6c451d4fe5e5cc
ksh-beginner <ohk9134@naver.com>
2022-12-16 00:15:39 +0900

    feat(InputView) : 게임 재시도/종료 커맨드 입력 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -7,7 +7,7 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputView {
 
-    private InputValidator inputValidator;
+    private final InputValidator inputValidator;
 
     public InputView() {
         inputValidator = new InputValidator();
@@ -34,7 +34,8 @@ public class InputView {
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public String readRetryOrEndCommand() {
+        String retryOrEndCommand = Console.readLine();
+        return retryOrEndCommand;
     }
 }
