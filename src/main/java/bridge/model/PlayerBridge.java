60ffe946e3f7eb45728f8abadabec6d8a1c1a95a
ksh-beginner <ohk9134@naver.com>
2022-12-15 21:53:35 +0900

    feat(BridgeGame) : 플레이어 이동 기능 구현

---
+++ src/main/java/bridge/model/PlayerBridge.java
@@ -0,0 +1,38 @@
+package bridge.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerBridge {
+
+    private static final String UP_MOVING = "U";
+    private static final String DOWN_MOVING = "D";
+    private static final String SUCCESS_SHAPE = "O";
+    private static final String FAILURE_SHAPE = "X";
+    private static final String BLANK_SHAPE = " ";
+
+    private final List<String> upShape = new ArrayList<>();
+    private final List<String> downShape = new ArrayList<>();
+
+    public void addSuccessShape(String moving) {
+        if (moving.equals(UP_MOVING)) {
+            upShape.add(SUCCESS_SHAPE);
+            downShape.add(BLANK_SHAPE);
+        }
+        if (moving.equals(DOWN_MOVING)) {
+            downShape.add(SUCCESS_SHAPE);
+            upShape.add(BLANK_SHAPE);
+        }
+    }
+
+    public void addFailureShape(String moving) {
+        if (moving.equals(UP_MOVING)) {
+            upShape.add(FAILURE_SHAPE);
+            downShape.add(BLANK_SHAPE);
+        }
+        if (moving.equals(DOWN_MOVING)) {
+            downShape.add(FAILURE_SHAPE);
+            upShape.add(BLANK_SHAPE);
+        }
+    }
+}
--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -3,6 +3,7 @@ package bridge.service;
 import bridge.BridgeMaker;
 import bridge.BridgeRandomNumberGenerator;
 import bridge.model.GeneratedBridge;
+import bridge.model.PlayerBridge;
 
 import java.util.List;
 
@@ -13,9 +14,17 @@ public class BridgeGame {
 
     private final BridgeMaker bridgeMaker;
     private GeneratedBridge generatedBridge;
+    private final PlayerBridge playerBridge;
+
+    private static final String UP_MOVING = "U";
+    private static final String DOWN_MOVING = "D";
+
+    private int moveIndex;
 
     public BridgeGame() {
         this.bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        this.playerBridge = new PlayerBridge();
+        moveIndex = 0;
     }
 
     public void generateBridge(int bridgeSize) {
@@ -28,7 +37,14 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String moving) {
+        if (generatedBridge.canMove(moveIndex, moving)) {
+            playerBridge.addSuccessShape(moving);
+            moveIndex++;
+        }
+        if (!generatedBridge.canMove(moveIndex, moving)) {
+            playerBridge.addFailureShape(moving);
+        }
     }
 
     /**
